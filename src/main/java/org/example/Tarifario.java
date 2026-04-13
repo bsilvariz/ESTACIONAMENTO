af736c1a10242f301d7677206d0575339ab4a9b3
b.silva <bianca.silva.143@ad.ufrn.br>
2026-04-13 14:37:15 -0300

    Projeto limpo

---
+++ src/main/java/org/example/Tarifario.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Tarifario {
+    private double valorHora;
+    private double valorDaria;
+    private double valorSemana;
+    private double valorMes;
+
+
+}
